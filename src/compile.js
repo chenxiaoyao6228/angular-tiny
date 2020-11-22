0c034b17bde7461914d5efb1a85d9fb09331cd6c
chenxiaoyao <chenxiaoyao6228@163.com>
2020-11-22 23:58:23 +0800

    allows creating many directives with the same name

--- src/compile.js
+++ src/compile.js
@@ -1,6 +1,17 @@
 export default function $CompileProvider($provide) {
+  let hasDirectives = {}
   this.directive = function(name, directiveFactory) {
-    $provide.factory(name + 'Directive', directiveFactory)
+    if (!Object.prototype.hasOwnProperty.call(hasDirectives, name)) {
+      hasDirectives[name] = []
+      $provide.factory(name + 'Directive', [
+        '$injector',
+        function($injector) {
+          let factories = hasDirectives[name]
+          return factories.map($injector.invoke)
+        }
+      ])
+    }
+    hasDirectives[name].push(directiveFactory)
   }
   this.$get = function() {}
 }
--- test/compile.spec.js
+++ test/compile.spec.js
@@ -12,4 +12,21 @@ describe('$compile', () => {
     let injector = createInjector(['ng', 'myModule'])
     expect(injector.has('testingDirective')).toBe(true)
   })
+  it('allows creating many directives with the same name', () => {
+    let myModule = window.angular.module('myModule', [])
+    myModule.directive('testing', () => ({ d: 'one' }))
+    myModule.directive('testing', () => ({ d: 'two' }))
+    let injector = createInjector(['ng', 'myModule'])
+    let result = injector.get('testingDirective')
+    expect(result.length).toBe(2)
+    expect(result[0].d).toEqual('one')
+    expect(result[1].d).toEqual('two')
+  })
+  it('does not allow a directive called hasOwnProperty', () => {
+    let myModule = window.angular.module('myModule', [])
+    myModule.directive('hasOwnProperty', () => {})
+    expect(() => {
+      createInjector(['ng', 'myModule'])
+    }).toThrow()
+  })
 })
